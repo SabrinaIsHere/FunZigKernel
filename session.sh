3581b70548c6018c18fbd24e9013bc8d8d270fa1
Sabrina Myers <sunseed218@gmail.com>
2026-03-26 09:42:54 -0600

    tmux session script

---
+++ session.sh
@@ -0,0 +1,15 @@
+# https://www.geeksforgeeks.org/linux-unix/how-to-create-tmux-session-with-a-script/
+
+SESSION="ZigKernel"
+PROJECT="~/Projects/Zig/FunZigKernel"
+
+# Check if the session already exists
+if ! tmux has-session -t $SESSION 2>/dev/null; then
+    tmux new-session -d -n "Code" -c "$PROJECT/src" -s "$SESSION"
+    tmux send-keys -t "$SESSION:0" "nvim $PROJECT/src/main.zig" C-m
+    
+	tmux new-window -n "Build" -t "$SESSION"
+    tmux send-keys -t "$SESSION:1" "ls" C-m
+fi
+
+tmux attach-session -t "$SESSION_NAME:0"
